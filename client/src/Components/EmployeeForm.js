da407d57f6f25c3d813c24627dcf045d84dd4fd5
babylee2002 <babylee2002@gmail.com>
2018-10-11 15:05:17 -0400

    employee edit works

--- client/src/Components/EmployeeForm.js
+++ client/src/Components/EmployeeForm.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class EmployeeForm extends Component {
-
-    state = {
-        birth_date: '',
-        first_name: '',
-        last_name: '',
-        gender: '',
-        title: ''
+    constructor(props) {
+        super(props)
+        const { employee } = props
+        this.state = {
+            birth_date: employee ? employee.birth_date : '',
+            first_name: employee ? employee.first_name : '',
+            last_name: employee ? employee.last_name : '',
+            gender: employee ? employee.gender : '',
+            title: employee ? employee.title : '',
+        }
     }
 
-    handleSubmit = () => {
 
+    handleSubmit = () => {
+        const { birth_date, first_name, last_name, gender, title } = this.state
+        const { employee } = this.props
+        const payload = { birth_date, first_name, last_name, gender, title }
+        axios.put(`/api/employees/${employee.id}`, payload)
     }
 
     handleChange = name => e => {
@@ -19,30 +27,31 @@ export default class EmployeeForm extends Component {
     }
 
     render() {
+        const { birth_date, first_name, last_name, gender, title } = this.state
         return (
-            <div>
+            <div className='container'>
                 <form className='form' onSubmit={this.handleSubmit}>
                     <div>
                         <label>Birth Date</label>
-                        <input onChange={this.handleChange('birth_date')} />
+                        <input value={birth_date} onChange={this.handleChange('birth_date')} />
                     </div>
                     <div>
                         <label>First Name</label>
-                        <input onChange={this.handleChange('first_name')} />
+                        <input value={first_name} onChange={this.handleChange('first_name')} />
                     </div>
                     <div>
                         <label>Last Name</label>
-                        <input onChange={this.handleChange('last_name')} />
+                        <input value={last_name} onChange={this.handleChange('last_name')} />
                     </div>
                     <div>
                         <label>Gender</label>
-                        <input onChange={this.handleChange('gender')} />
+                        <input value={gender} onChange={this.handleChange('gender')} />
                     </div>
                     <div>
                         <label>Title</label>
-                        <input onChange={this.handleChange('title')} />
+                        <input value={title} onChange={this.handleChange('title')} />
                     </div>
-                    <button>Submit</button>
+                    <button className='btn btn-primary'>Submit</button>
                 </form>
 
             </div>
--- client/src/Components/EmployeePage.js
+++ client/src/Components/EmployeePage.js
@@ -2,26 +2,28 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
+import EmployeeForm from './EmployeeForm'
+
 export default class EmployeeList extends Component {
 
     state = {
-        employee: {}
+        employee: {},
+        edited: false
     }
 
     componentDidMount = () => {
         const id = this.props.match.params.employeeId
-        console.log(id)
         axios.get(`/api/employees/${id}`)
             .then(res => this.setState({ employee: res.data }))
+        console.log(this.state.employee)
     }
 
-    handleEdit = (id) => {
-        axios.put(`/api/employees/${id}`)
+    handleEdit = () => {
+        this.setState({ edited: !this.state.edited, employee: this.state.employee })
     }
 
     render() {
-        const { employee } = this.state
-        console.log(employee)
+        const { employee, edited } = this.state
         return (
             <div className='container'>
                 <h2>Employee Information</h2>
@@ -33,7 +35,7 @@ export default class EmployeeList extends Component {
                         <th>Last Name</th>
                         <th>Gender</th>
                         <th>Title</th>
-                        <th>Delete</th>
+                        <th>Edit</th>
                     </tr>
 
                     <tr>
@@ -43,11 +45,11 @@ export default class EmployeeList extends Component {
                         <td>{employee.last_name}</td>
                         <td>{employee.gender}</td>
                         <td>{employee.title}</td>
-                        <td><button className='btn btn-danger' onClick={() => this.handleDelete(employee.id)}>Edit</button></td>
+                        <td><button className='btn btn-danger' onClick={this.handleEdit}>Edit</button></td>
                     </tr>
                 </table>
                 <hr />
-
+                {edited ? <EmployeeForm employee={employee} /> : null}
             </div>
         )
     }
